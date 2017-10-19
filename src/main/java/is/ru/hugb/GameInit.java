307d19bbefa198063087252489d2aa7524f3ea6c
Sigvaldi Kárason <sigvaldi22@gmail.com>
2017-10-19 16:42:53 +0000

    Added wrapper

--- src/main/java/is/ru/hugb/GameInit.java
+++ src/main/java/is/ru/hugb/GameInit.java
@@ -7,6 +7,18 @@ public class GameInit{
     }
 
 	public static void main(String[] args){
-		System.out.println(new GameInit().getGreeting());		
+		System.out.println(new GameInit().getGreeting());
+
+		//initialize an array of arrays to reprisent the playing grid from parameters 
+
+		//Repeat until winner is found
+			//player makes a turn 
+			//update playing grid
+			//check for winner
+			//set next player
+
+		//diplay winner
+
+		//give option to play again
 	}
 }
