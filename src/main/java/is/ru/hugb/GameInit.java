b1ae4ba5f456331e0d37571752b9586f79d7e3a5
KristjanEinarsson <kristjanein15@ru.is>
2017-10-19 16:47:12 +0000

    Tók strenginn út

--- src/main/java/is/ru/hugb/GameInit.java
+++ src/main/java/is/ru/hugb/GameInit.java
@@ -2,10 +2,6 @@ package is.ru.hugb;
 
 public class GameInit{
 
-	public String getGreeting() {
-        return "Hello world.";
-    }
-
 	public static void main(String[] args){
 		System.out.println(new GameInit().getGreeting());
 
